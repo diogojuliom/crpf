9a0603a20dc55bbabb2f59dbe7fba381830c72ea
wemerson <wemerson@uptimegroup.local>
2015-10-23 10:45:55 -0200

    tela de login atual

---
+++ js/site.js
@@ -0,0 +1,9 @@
+$("a[href*=#]").click(function(){
+	event.preventDefault();
+});
+$("a#efetuarCadastro").click(function(){
+	$("#cadastro").css('display', 'block');
+});
+$(document).ready(function(){
+	
+});
